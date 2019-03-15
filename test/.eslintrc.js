6004b6c5beaea1ac277c2f611afe27a8376a886f
psaint-j <psaint-j@student.42.fr>
2019-03-15 17:28:18 +0100

    remove files

--- test/.eslintrc.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  env: {
-    jest: true,
-    es6: true,
-  }
-};
--- test/setupDb.js
+++
@@ -1,9 +0,0 @@
-#!/bin/node
-const { sequelize } = require('../models');
-
-const setupDb = async () => {
-  await sequelize.sync({ force: true });
-  process.exit(0);
-};
-
-setupDb();
